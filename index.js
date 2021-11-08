b0ef75b95251ad70f686e5a44d387cdd7194ec58
SAHEBDHARA <sahendhara961@gmail.com>
2021-11-08 20:48:49 +0530

    uploading for first time

---
+++ index.js
@@ -0,0 +1,6 @@
+let seen = document.getElementById("error");
+//console.log(seen);
+function buyNow(){
+  console.log(seen);
+  seen.innerHTML = " This product is not avalable, please try again! ";
+}
